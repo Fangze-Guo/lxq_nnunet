ba2d88348bd1dbbdd9bab8f982cf7bb30e370138
Fangze-Guo <997167305@qq.com>
2025-11-20 16:57:07 +0800

    删除测试脚本

--- myfunction_1/test_cuda.py
+++
@@ -1,4 +0,0 @@
-import torch
-print(torch.cuda.is_available())  # 检查 CUDA 是否可用
-print(torch.cuda.device_count())  # 查看有多少个 GPU 可用
-print(torch.cuda.get_device_name(0))  # 如果有多个 GPU，查看第一个 GPU 名字
--- myfunction_1/正交变换.py
+++
@@ -1,7 +0,0 @@
-import SimpleITK as sitk
-
-# SimpleITK 对方向矩阵的处理更宽松
-image = sitk.ReadImage("/media/dell/T7 Shield/nnunet/AllData/PKUShZh/1489231.nii.gz")
-
-# 保存为新的NIFTI文件
-sitk.WriteImage(image, "/media/dell/T7 Shield/nnunet/AllData/PKUShZh/1489231_new.nii.gz")
